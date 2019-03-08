b6a2dd2562103914cb97cfa77a41f4195e028c23
blinkous <saradhani1@gmail.com>
2019-03-08 18:45:11 -0500

    added scaling and rotation for kart iamges

--- MarioKartGestures/MarioKartViewController.swift
+++ MarioKartGestures/MarioKartViewController.swift
@@ -17,19 +17,41 @@ class MarioKartViewController: UIViewController {
     }
     
     
-    
+    // Move the kart
     @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
         // In order to drag the kart around we need the location
         let location = sender.location(in: view)
-//        print("Location: x: \(location.x), y: \(location.y)")
+        print("Location: x: \(location.x), y: \(location.y)")
 
         // We will use the location to move the kart
         let kartView = sender.view!
         kartView.center = location
     }
     
-    
+    // Scale the kart
     @IBAction func didPinchKart(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        print("scale: \(scale)")
+        
+        // Scale the kart
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+
     }
     
+    //Rotate the kart
+    @IBAction func didRotateKart(_ sender: UIRotationGestureRecognizer) {
+        let rotation = sender.rotation
+        print("rotation: \(rotation)")
+        
+        // Rotate the kart
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(rotationAngle: rotation)
+
+    }
+    
+    // Tapped the kart to animate it
+    @IBAction func didTapKartVIew(_ sender: UITapGestureRecognizer) {
+        
+    }
 }
