0c39c3bc6ee903eccf22460b5b9b3ba16f7c175e
blinkous <saradhani1@gmail.com>
2019-03-08 18:05:22 -0500

    First Commit

--- MarioKartGestures/MarioKartViewController.swift
+++ MarioKartGestures/MarioKartViewController.swift
@@ -16,15 +16,15 @@ class MarioKartViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        // In order to drag the kart around we need the location
+        let location = sender.location(in: view)
+        
+        print("Location: x: \(location.x), y: \(location.y)")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Lab Project
+# Mario Kart Gestures Lab Project
+
+iOS app that allows users to interact with Mario Kart characters.
